97c8549173ad57398b01c0250eac829054cc0236
qybit <2601970213@qq.com>
2023-01-21 19:48:12 +0800

    feat:lecture1

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by wqy on 2023/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            Text("Hello cs193p!")
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by wqy on 2023/1/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
